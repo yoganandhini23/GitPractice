9b2250d9367ac68a536f2f3c3f54f5af9f98588a
Yoganandhini23 <yoganandhini23@gmail.com>
2019-02-01 21:51:12 +0000

    I am checking revert

--- Gitlearning/src/main/java/LearningNewProGit/Gitlearning/Googleit.java
+++ Gitlearning/src/main/java/LearningNewProGit/Gitlearning/Googleit.java
@@ -21,7 +21,7 @@ public class Googleit {
 		// Maximize browser
 		 
 		//driver.manage().window().maximize();
-		System.out.println("Nandhini");
+		System.out.println("Checking revert1");
 		
 		 WebElement element = driver.findElement(By.name("q"));
          element.sendKeys("Cheese!");
