51c37faf525cff4c7d07959f4653231066c119cf
Yoganandhini Shanmugam <Yoganandhinis@cccs.co.uk>
2019-02-01 15:18:47 +0000

    Iwant to add my code

---
+++ Gitlearning/src/main/java/LearningNewProGit/Gitlearning/Googleit.java
@@ -0,0 +1,32 @@
+package LearningNewProGit.Gitlearning;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class Googleit {
+
+	public static void main(String[] args) {
+		 
+		System.setProperty("webdriver.chrome.driver", "D:\\Driver\\chrome\\chromedriver.exe");
+		 
+		// Initialize browser
+		WebDriver driver=new ChromeDriver();
+		 
+		// Open facebook
+		driver.get("http://www.google.com");
+		 
+		// Maximize browser
+		 
+		//driver.manage().window().maximize();
+		System.out.println("hello");
+		
+		 WebElement element = driver.findElement(By.name("q"));
+         element.sendKeys("Cheese!");
+         element.submit();
+
+	}
+
+}
